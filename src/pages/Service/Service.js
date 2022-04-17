33b5a102ac63d2e85fd82a6a4f46268c0f6d1c77
dev-weirdo <dev_weirdo@protonmail.com>
2022-04-17 20:07:12 +0600

    Banner added

--- src/pages/Service/Service.js
+++ src/pages/Service/Service.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Service = ({ service }) => {
   const { name, price, img, benefits } = service
-  return <div>{name}</div>
+  return (
+    <div>
+      <p>{name}</p>
+      <p>{benefits}</p>
+      <p>{price}</p>
+      <img src={img} alt='' />
+    </div>
+  )
 }
 
 export default Service
