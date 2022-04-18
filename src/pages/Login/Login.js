77356e20b314e95035fff5170aba202818317a25
dev-weirdo <dev_weirdo@protonmail.com>
2022-04-18 14:52:23 +0600

    Email Verification added

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,5 +1,5 @@
 import React, { useRef } from 'react'
-import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth'
+import { useSendPasswordResetEmail, useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth'
 import { Link, useLocation, useNavigate } from 'react-router-dom'
 import auth from '../../firebase.init'
 import SocialLogin from '../../SocialLogin/SocialLogin'
@@ -25,11 +25,55 @@ const Login = () => {
     signInWithEmailAndPassword(email, password);
   }
 
+  const [sendPasswordResetEmail, sending, resetError] = useSendPasswordResetEmail(auth)
+
+  const handlePasswordReset = async () => {
+    const email = emailRef.current.value;
+    await sendPasswordResetEmail(email)
+
+  }
+
+
+
   const from = location.state?.from?.pathname || '/'
 
   if (user) {
     navigate(from, { replace: true })
   }
+  if (sending) {
+    return (
+      <>
+        <div id="toast-simple" className="flex items-center w-full max-w-xs p-4 space-x-4 text-gray-500 bg-white divide-x divide-gray-200 rounded-lg shadow dark:text-gray-400 dark:divide-gray-700 space-x dark:bg-gray-800" role="alert">
+          <svg className="w-5 h-5 text-blue-600 dark:text-blue-500" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="paper-plane" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M511.6 36.86l-64 415.1c-1.5 9.734-7.375 18.22-15.97 23.05c-4.844 2.719-10.27 4.097-15.68 4.097c-4.188 0-8.319-.8154-12.29-2.472l-122.6-51.1l-50.86 76.29C226.3 508.5 219.8 512 212.8 512C201.3 512 192 502.7 192 491.2v-96.18c0-7.115 2.372-14.03 6.742-19.64L416 96l-293.7 264.3L19.69 317.5C8.438 312.8 .8125 302.2 .0625 289.1s5.469-23.72 16.06-29.77l448-255.1c10.69-6.109 23.88-5.547 34 1.406S513.5 24.72 511.6 36.86z"></path></svg>
+          <div className="pl-4 text-sm font-normal">Message sent successfully.</div>
+        </div>
+      </>
+    )
+  }
+  if (resetError) {
+
+  }
+
+  if (loading) {
+    return (
+      <>
+        <div className='w-full flex justify-center items-center'>
+          <svg role="status" className="mr-2 w-16 h-16 text-gray-200 animate-spin dark:text-gray-600 fill-white" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"></path>
+            <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"></path>
+          </svg>
+        </div>
+      </>
+    )
+  }
+  let errMsg;
+  if (error) {
+    errMsg = <div>
+      <p className='text-red-600 border-2 rounded-md my-2 py-1 px-2'>Error: {error.message}</p>
+    </div>
+
+  }
+
   return (
     <div className='mx-auto my-16 p-4 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md sm:p-6 lg:p-8 dark:bg-gray-800 dark:border-gray-700'>
       <form className='space-y-6' onSubmit={handleLogin}>
@@ -71,12 +115,7 @@ const Login = () => {
           ></input>
         </div>
         <div className='flex items-start'>
-          <p
-            href='#'
-            className='ml-auto text-sm text-blue-700 hover:underline dark:text-blue-500'
-          >
-            Lost Password?
-          </p>
+          <button onClick={handlePasswordReset} className='ml-auto text-sm text-blue-700 hover:underline dark:text-blue-500'>Lost Password?</button>
         </div>
         <button
           type='submit'
@@ -84,6 +123,7 @@ const Login = () => {
         >
           Login to your account
         </button>
+        {errMsg}
         <div className='text-sm font-medium text-gray-500 dark:text-gray-300 flex justify-between items-center'>
           Not registered?
           <Link to='/register' className='text-blue-700 hover:underline dark:text-blue-500'>Create Account</Link>
